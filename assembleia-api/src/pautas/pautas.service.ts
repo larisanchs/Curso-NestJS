7c0e8645361ec1f092dbf91ad01db977919a1163
larisanchs <larissasanches952@gmail.com>
2026-03-08 00:51:42 -0300

    Criando a lógica de inicialização de sessão

--- assembleia-api/src/pautas/pautas.service.ts
+++ assembleia-api/src/pautas/pautas.service.ts
@@ -5,6 +5,10 @@ import { Result } from 'src/common/result';
 
 @Injectable()
 export class PautasService {
+
+    // Criando constante que armazena o tempo padrão da pauta
+    static TEMPO_PADRAO_PAUTA: number = 10;
+
     constructor(
         @Inject('PAUTA_REPOSITORY')
         private readonly  pautaRepository: Repository<Pauta>
@@ -33,5 +37,25 @@ export class PautasService {
     // Criando um método assíncrono que vai retornar o array de pautas
     async findAll(): Promise<Pauta[]> {
         return await this.pautaRepository.find();
+    }
+
+    async IniciarSessao(
+        pauta: Pauta, 
+        minutos: number = PautasService.TEMPO_PADRAO_PAUTA) : Promise<boolean> {
+        
+        if(!pauta.isPossivelIniciarSessao()){
+            return false;
+        }
+
+        // Está definindo a abertura como a data e hora atual
+        pauta.abertura = new Date();
+
+        // Define que a data e hora de fechamento será a data e hora que foram definidas na abertura, somando os minutos definidos anteriormente (10 minutos) através do método "getTime()", mas esse método retorna a hora em milissegundos, por isso devemos multiplicar por 60000 (para converter para minutos).
+        pauta.fechamento = new Date( pauta.abertura.getTime() + minutos * 60000 );
+
+        // Chamando o repository para atualizar a pauta
+        await this.pautaRepository.update(pauta.id, pauta);
+
+        return true;
     }
 }
